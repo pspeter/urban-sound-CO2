0b9bc99a17c51e0b711f848d383869742840f008
peter-schmidbauer <peter.schmidb@gmail.com>
2018-05-02 22:57:33 +0200

    implements #5: model params configurable

--- urbansound/main.py
+++ urbansound/main.py
@@ -1,31 +1,33 @@
+import tensorflow as tf
+from keras import backend as K
+
 from model import MLPModel, CNNModel, LSTMModel
 from preprocessing import UrbanSoundData, UrbanSoundExtractor
-import logging
-from keras import backend as K
-import tensorflow as tf
-
 
 if __name__ == "__main__":
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    config.gpu_options.per_process_gpu_memory_fraction = 0.1
+    print(config.gpu_options.per_process_gpu_memory_fraction)
+    config.gpu_options.per_process_gpu_memory_fraction = 0.2
     K.set_session(tf.Session(config=config))
 
-    logger = logging.getLogger()
-    logger.setLevel("WARN")
     UrbanSoundExtractor().prepare_data()
     data = UrbanSoundData()
 
-    #mlp = MlpModel(data)
-    #mlp.train(epochs=100, verbose=0)
-    #mlp.visualize_training()
+    # mlp = MLPModel(data, [512, 512])
+    # mlp.train(batch_size=64, epochs=10, verbose=1)
+    # print(mlp.evaluate())
+    # mlp.visualize_training()
+    # del mlp
 
-    cnn = CNNModel(data)
-    cnn.train(batch_size=8, epochs=50, verbose=1)
+    cnn = CNNModel(data, [64, 128], dropout_probabilities=[0.5, 0.25])
+    cnn.train(batch_size=8, epochs=10, verbose=1)
     print(cnn.evaluate())
     cnn.visualize_training()
+    del cnn
 
-    #lstm = LSTMModel(data)
-    #lstm.train(batch_size=8, epochs=50, verbose=1)
-    #print(lstm.evaluate())
-    #lstm.visualize_training()
+    lstm = LSTMModel(data, [64])
+    lstm.train(batch_size=8, epochs=1, verbose=1)
+    print(lstm.evaluate())
+    lstm.visualize_training()
+    del lstm
--- urbansound/model.py
+++ urbansound/model.py
@@ -1,5 +1,4 @@
-import logging
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Sequence
 
 import numpy as np
 from keras.callbacks import EarlyStopping
@@ -17,14 +16,26 @@ class NotTrainedError(Exception):
 
 
 class BaseModel:
-    def __init__(self, data: UrbanSoundData):
+    """Base Model for """
+
+    def __init__(self, data: UrbanSoundData,
+                 hidden_layer_sizes: Sequence[int],
+                 dropout_probabilities: Optional[Sequence[Optional[int]]] = None,
+                 use_batch_norm: bool = True):
+        if dropout_probabilities is not None and \
+                len(hidden_layer_sizes) != len(dropout_probabilities):
+            raise ValueError("Length of hidden_layer_sizes and dropout_probabilities need to be the same")
+
         self.data = data
+        self.hidden_layer_sizes = hidden_layer_sizes
+        self.dropout_probabilities = dropout_probabilities if dropout_probabilities else \
+            [None] * len(hidden_layer_sizes)
+        self.use_batch_norm = use_batch_norm
         self.model: Sequential = None
         self.history = None
 
     def train(self, batch_size: Optional[int] = 32, epochs: Optional[int] = 10,
-              short_data: Optional[bool] = False, verbose: int=0):
-
+              short_data: Optional[bool] = False, verbose: int = 0):
         if short_data:
             train_features, test_features, train_labels, test_labels = self.data.train_data_short
         else:
@@ -38,7 +49,6 @@ class BaseModel:
 
         early_stop_callback = EarlyStopping(patience=20, verbose=1)
 
-        logging.info("Start training")
         self.history = self.model.fit(train_features, train_labels,
                                       epochs=epochs,
                                       batch_size=batch_size,
@@ -59,6 +69,7 @@ class BaseModel:
     def predict(self):
         if self.model is None:
             raise NotTrainedError("Model needs to be trained before prediction")
+
         raise NotImplementedError()
 
     def visualize_training(self):
@@ -90,15 +101,31 @@ class BaseModel:
 
 
 class MLPModel(BaseModel):
-    def __init__(self, data: UrbanSoundData):
-        super().__init__(data)
+    def __init__(self, data: UrbanSoundData,
+                 hidden_layer_sizes: Sequence[int],
+                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
+                 use_batch_norm: bool = True):
+        super().__init__(data, hidden_layer_sizes,
+                         dropout_probabilities, use_batch_norm)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
-        model.add(Dense(64, input_shape=input_shape, activation="relu"))
-        model.add(BatchNormalization())
-        model.add(Dense(64, activation="relu"))
-        model.add(BatchNormalization())
+
+        for layer_idx, (layer_size, dropout_proba) in enumerate(
+                zip(self.hidden_layer_sizes, self.dropout_probabilities)):
+            if layer_idx == 0:
+                model.add(Dense(layer_size, input_shape=input_shape, activation=None))
+            else:
+                model.add(Dense(layer_size, activation=None))
+
+            if self.use_batch_norm:
+                model.add(BatchNormalization())
+
+            model.add(Activation("relu"))
+
+            if dropout_proba:
+                model.add(Dropout(dropout_proba))
+
         model.add(Dense(NUM_CLASSES, activation="softmax"))
         model.compile(optimizer="adam",
                       loss="categorical_crossentropy",
@@ -111,29 +138,51 @@ class MLPModel(BaseModel):
 
 
 class CNNModel(BaseModel):
-    def __init__(self, data: UrbanSoundData):
-        super().__init__(data)
+    """Simplified Interface to Keras CNN Models
+    The last layer will always be fully connected, all other layers will include a Conv2D layer
+    with tanh activation and a MaxPooling2D layer."""
+
+    def __init__(self, data: UrbanSoundData,
+                 hidden_layer_sizes: Sequence[int],
+                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
+                 use_batch_norm: bool = True):
+        if len(hidden_layer_sizes) < 2:
+            raise ValueError("CNNModel needs at least two hidden layers (last layer is fully connected)")
+        super().__init__(data, hidden_layer_sizes,
+                         dropout_probabilities, use_batch_norm)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
-        model.add(Conv2D(64, kernel_size=(3, 3), padding="same",
-                         input_shape=input_shape, activation="relu"))
-        model.add(BatchNormalization())
-        model.add(MaxPooling2D())
 
-        model.add(Conv2D(64, kernel_size=(3, 3), padding="same",
-                         input_shape=input_shape, activation="relu"))
-        model.add(BatchNormalization())
-        model.add(MaxPooling2D())
+        for layer_idx, (layer_size, dropout_proba) in enumerate(
+                zip(self.hidden_layer_sizes[:-1], self.dropout_probabilities[:-1])):
+            if layer_idx == 0:
+                model.add(Conv2D(layer_size, (3, 3), padding="same",
+                                 input_shape=input_shape, activation=None))
+            else:
+                model.add(Conv2D(layer_size, (3, 3), padding="same",
+                                 activation=None))
 
-        model.add(Conv2D(64, kernel_size=(3, 3), padding="same",
-                         input_shape=input_shape, activation="relu"))
-        model.add(BatchNormalization())
-        model.add(MaxPooling2D())
+            if self.use_batch_norm:
+                model.add(BatchNormalization())
+
+            model.add(Activation("relu"))
+
+            if dropout_proba:
+                model.add(Dropout(dropout_proba))
+
+            model.add(MaxPooling2D())
 
         model.add(Flatten())
-        model.add(Dense(512, use_bias=False, activation="relu"))
-        model.add(BatchNormalization())
+        model.add(Dense(self.hidden_layer_sizes[-1], use_bias=False, activation=None))
+
+        if self.use_batch_norm:
+            model.add(BatchNormalization())
+
+        model.add(Activation("relu"))
+
+        if self.dropout_probabilities[-1]:
+            model.add(Dropout(self.dropout_probabilities[-1]))
 
         model.add(Dense(NUM_CLASSES, activation="softmax"))
         model.compile(optimizer="adam",
@@ -149,16 +198,32 @@ class CNNModel(BaseModel):
 
 
 class LSTMModel(BaseModel):
-    def __init__(self, data: UrbanSoundData):
-        super().__init__(data)
+    def __init__(self, data: UrbanSoundData,
+                 hidden_layer_sizes: Sequence[int],
+                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None):
+        # LSTMs do not work well with batch norm
+        super().__init__(data, hidden_layer_sizes,
+                         dropout_probabilities, use_batch_norm=False)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
 
-        model.add(LSTM(512, input_shape=input_shape, return_sequences=True))
+        for layer_idx, (layer_size, dropout_proba) in enumerate(
+                zip(self.hidden_layer_sizes[:-1], self.dropout_probabilities[:-1])):
+            if layer_idx == 0:
+                model.add(LSTM(layer_size, return_sequences=True,
+                               input_shape=input_shape))
+            else:
+                model.add(LSTM(layer_size, return_sequences=True))
 
-        model.add(LSTM(512, input_shape=input_shape))
-        model.add(Dropout(0.25))
+            if dropout_proba:
+                model.add(Dropout(dropout_proba))
+        if len(self.hidden_layer_sizes) == 1:
+            model.add(LSTM(self.hidden_layer_sizes[-1], input_shape=input_shape))
+        else:
+            model.add(LSTM(self.hidden_layer_sizes[-1]))
+        if self.dropout_probabilities[-1]:
+            model.add(Dropout(self.dropout_probabilities[-1]))
 
         model.add(Dense(NUM_CLASSES, activation="softmax"))
         model.compile(optimizer="adam",
@@ -170,6 +235,4 @@ class LSTMModel(BaseModel):
         """For LSTMs, the input should be of shape (batch_size, time steps, input_dim).
         That means we need to swap the 2nd and 3rd axis.
         """
-        print(np.swapaxes(features, 1, 2).shape)
         return np.swapaxes(features, 1, 2)
-
