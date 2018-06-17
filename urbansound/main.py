a052eaefe4c92113236a8dffaf42a693e45cd14c
peter-schmidbauer <peter.schmidb@gmail.com>
2018-06-17 22:55:14 +0200

    lots of changes
    add timercallback, automatic metric logging, save model, log tensorboard, overwrite flags, a SimpleRNN Model and the test setup for final evaluation

--- urbansound/main.py
+++ urbansound/main.py
@@ -1,7 +1,54 @@
 from preprocessing import UrbanSoundData
 
+
+def all_test_runs():
+    layer_configs = [
+        [16, 128],
+        [16, 16, 16, 128],
+        [16, 32, 64, 128],
+    ]
+
+    for layer_config in layer_configs:
+        for dropout in [None, 0.5]:
+            for batch_norm in [True, False]:
+                run_test(data, CNNModel, layer_config, dropout, batch_norm)
+
+    layer_configs = [
+        [512],
+        [32, 32, 32, 32, 32, 32],
+        [32, 64, 128, 256],
+        [256, 128, 64, 32],
+    ]
+
+    for layer_config in layer_configs:
+        for dropout in [None, 0.5]:
+            for batch_norm in [True, False]:
+                run_test(data, MLPModel, layer_config, dropout, batch_norm)
+
+    layer_configs = [
+        [128],
+        [32, 32, 32, 32],
+        [64, 32],
+        [32, 64],
+    ]
+
+    for layer_config in layer_configs:
+        for dropout in [0, 0.5]:
+                run_test(data, LSTMModel, layer_config, dropout, False)
+
+
+def run_test(data, model_type, layer_config, dropout, batch_norm):
+    print(model_type.__name__, layer_config, dropout, batch_norm)
+    dropouts = [dropout for _ in range(len(layer_config))]
+
+    for _ in range(5):
+        model = model_type(data, layer_config, dropouts, batch_norm,
+                           model_type.__name__, save_model=False)
+        model.train(batch_size=32, epochs=20, verbose=0)
+        model.evaluate(log_dir="../exploration/model_log.txt")
+
+
 if __name__ == "__main__":
-    from model import MLPModel, CNNModel, LSTMModel
     import tensorflow as tf
     from keras import backend as K
 
@@ -9,22 +56,16 @@ if __name__ == "__main__":
     config.gpu_options.allow_growth = True
     K.set_session(tf.Session(config=config))
 
-    data = UrbanSoundData(n_mfccs=20, n_augmentations=1)
+    from model import MLPModel, CNNModel, LSTMModel
 
-    # mlp = MLPModel(data, [512, 512], [0.7, 0.5)
-    # mlp.train(batch_size=64, epochs=50, verbose=1)
-    # print(mlp.evaluate())
-    # mlp.visualize_training()
-    # del mlp
+    data = UrbanSoundData(n_mfccs=20, n_augmentations=0)
 
-    cnn = CNNModel(data, [32, 64, 256, 512], dropout_probabilities=None, use_batch_norm=True)
+    # all_test_runs()
+
+    cnn = CNNModel(data, [64, 128, 512], dropout_probabilities=None, use_batch_norm=True,
+                   log_tensorboard=False, model_name="CNN", save_model=False, overwrite=True)
     cnn.train(batch_size=32, epochs=50, verbose=1)
-    print("Metrics on held out test data:", cnn.evaluate())
+    print("Evaluation:", cnn.evaluate())
     cnn.visualize_training()
     del cnn
 
-    # lstm = LSTMModel(data, [512], [0.25])
-    # lstm.train(batch_size=32, epochs=5, verbose=1)
-    # print(lstm.evaluate())
-    # lstm.visualize_training()
-    # del lstm
--- urbansound/model.py
+++ urbansound/model.py
@@ -1,12 +1,13 @@
 import json
-import warnings
+import shutil
+import time
 from datetime import datetime
 from pathlib import Path
 from typing import Tuple, Optional, Sequence
 
 import numpy as np
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Activation, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, Callback
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Activation, Dropout, SimpleRNN
 from keras.layers import Dense, LSTM, BatchNormalization
 from keras.models import Sequential, Model
 
@@ -19,10 +20,26 @@ class NotTrainedError(Exception):
     pass
 
 
+class TimerCallback(Callback):
+    def __init__(self):
+        super().__init__()
+        self.total_time = 0
+        self.epoch_start = None
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_start = time.time()
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.total_time += time.time() - self.epoch_start
+
+
 class BaseModel:
     def __init__(self, data: UrbanSoundData, hidden_layer_sizes: Sequence[int],
                  dropout_probabilities: Optional[Sequence[Optional[int]]] = None,
-                 use_batch_norm: bool = True, model_name: str = None):
+                 use_batch_norm: bool = True, model_name: str = None,
+                 log_tensorboard: bool = False, save_model: bool = True,
+                 overwrite: bool = False):
+
         if dropout_probabilities is not None and \
                 len(hidden_layer_sizes) != len(dropout_probabilities):
             raise ValueError("Length of hidden_layer_sizes and "
@@ -39,6 +56,12 @@ class BaseModel:
         self.name = model_name if model_name else \
             f"{self.__class__.__name__}_{now}".replace(":", "-")
 
+        self.log_tensorboard = log_tensorboard
+        self.overwrite = overwrite
+        self.save_model = save_model
+
+        self.train_seconds_per_sample = None
+
     def train(self, batch_size: Optional[int] = 32, epochs: Optional[int] = 10,
               verbose: int = 0):
 
@@ -52,47 +75,84 @@ class BaseModel:
 
         save_dir = Path("..", "model", f"{self.name}")
 
-        if save_dir.exists():
-            raise ValueError(f"Model with name {self.name} exists already")
-        else:
-            save_dir.mkdir(parents=True, exist_ok=True)
+        if self.save_model or self.log_tensorboard:
+            if save_dir.exists():
+                if not self.overwrite:
+                    raise ValueError(f"Model with name {self.name} exists already. Set overwrite=True "
+                                     f"on {self.__class__.__name__} to overwrite old model.")
+
+                shutil.rmtree(save_dir)
+
+            save_dir.mkdir(parents=True, exist_ok=False)
 
         save_path = save_dir / "weights.epoch{epoch:02d}-loss{val_categorical_accuracy:.2f}.hdf5"
-        save_callback = ModelCheckpoint(str(save_path), "val_categorical_accuracy", save_best_only=True)
+
         early_stop_callback = EarlyStopping(patience=10, verbose=1)
-        tensorboard_callback = TensorBoard(str(save_dir / "logs"), write_grads=True, write_images=True,
-                                           histogram_freq=5000)
+        timer_callback = TimerCallback()
+        callbacks = [early_stop_callback, timer_callback]
 
-        with open(save_dir / "model_structure.json", "w") as model_struc_file:
-            json.dump(self.model.to_json(), model_struc_file)
+        if self.save_model:
+            with open(save_dir / "model_structure.json", "w") as model_struc_file:
+                json.dump(self.model.to_json(), model_struc_file)
 
-        try:
-            self.history = self.model.fit(train_features, train_labels,
-                                          epochs=epochs,
-                                          batch_size=batch_size,
-                                          validation_data=(val_features, val_labels),
-                                          callbacks=[early_stop_callback, save_callback, tensorboard_callback],
-                                          verbose=verbose)
-        except KeyboardInterrupt:
-            print()
-            print("Early stopping (by user)")
+            save_callback = ModelCheckpoint(str(save_path), "val_categorical_accuracy", save_best_only=True)
+            callbacks.append(save_callback)
 
+        if self.log_tensorboard:
+            tensorboard_callback = TensorBoard(str(save_dir / "logs"), write_grads=True,
+                                               write_images=True, histogram_freq=1)
+            callbacks.append(tensorboard_callback)
+
+        self.history = self.model.fit(train_features, train_labels,
+                                      epochs=epochs,
+                                      batch_size=batch_size,
+                                      validation_data=(val_features, val_labels),
+                                      callbacks=callbacks,
+                                      verbose=verbose)
+
+        num_epochs = len(self.history.history["loss"])
+        self.train_seconds_per_sample = round(timer_callback.total_time /
+                                              num_epochs /
+                                              train_features.shape[0], 5)
         return self.history
 
-    def evaluate(self):
+    def evaluate(self, log_dir: str = None):
         if self.model is None:
             raise NotTrainedError("Model needs to be trained before evaluation")
 
-        features, labels = self.data.test_data
-        features = self._process_features(features)
+        train_features, _, train_labels, _ = self.data.train_data
+        test_features, test_labels = self.data.test_data
 
-        return dict(zip(self.model.metrics_names, self.model.evaluate(features, labels)))
+        train_features = self._process_features(train_features)
+        test_features = self._process_features(test_features)
 
-    def predict(self):
+        metrics = {
+            "test_acc": round(self.model.evaluate(test_features, test_labels, verbose=0)[1], 5),
+            "train_acc": round(self.model.evaluate(train_features, train_labels, verbose=0)[1], 5)
+        }
+
+        if log_dir:
+            log_dir = Path(log_dir)
+
+            if not log_dir.exists():
+                with open(log_dir, "a") as fp:
+                    fp.write("name,train_acc,test_acc,layer_sizes,dropout_rates,"
+                             "use_batch_norm,train_seconds_per_sample\n")
+
+            with open(log_dir, "a") as fp:
+                fp.write(f"{self.name},{metrics['train_acc']},{metrics['test_acc']}"
+                         f",'{self.hidden_layer_sizes}','{self.dropout_probabilities}',"
+                         f"{self.use_batch_norm},"
+                         f"{self.train_seconds_per_sample}\n")
+
+        return metrics
+
+    def predict(self, features: np.ndarray):
         if self.model is None:
             raise NotTrainedError("Model needs to be trained before prediction")
 
-        raise NotImplementedError()
+        features = self._process_features(features)
+        return self.data.inverse_transform(self.model.predict(features))
 
     def visualize_training(self):
         import matplotlib.pyplot as plt
@@ -126,9 +186,12 @@ class MLPModel(BaseModel):
     def __init__(self, data: UrbanSoundData,
                  hidden_layer_sizes: Sequence[int],
                  dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
-                 use_batch_norm: bool = True):
+                 use_batch_norm: bool = True, model_name: str = None,
+                 log_tensorboard: bool = False, save_model: bool = True,
+                 overwrite: bool = False):
         super().__init__(data, hidden_layer_sizes,
-                         dropout_probabilities, use_batch_norm)
+                         dropout_probabilities, use_batch_norm,
+                         model_name, log_tensorboard, save_model, overwrite)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
@@ -155,7 +218,7 @@ class MLPModel(BaseModel):
         return model
 
     def _process_features(self, features: np.ndarray) -> np.ndarray:
-        """Reshape 2D data into a 1D vector."""
+        """Reshape 2D data into 1D vectors."""
         return features.reshape(features.shape[0], -1)
 
 
@@ -167,11 +230,14 @@ class CNNModel(BaseModel):
     def __init__(self, data: UrbanSoundData,
                  hidden_layer_sizes: Sequence[int],
                  dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
-                 use_batch_norm: bool = True):
+                 use_batch_norm: bool = True, model_name: str = None,
+                 log_tensorboard: bool = False, save_model: bool = True,
+                 overwrite: bool = False):
         if len(hidden_layer_sizes) < 2:
             raise ValueError("CNNModel needs at least two hidden layers (last layer is fully connected)")
-        super().__init__(data, hidden_layer_sizes,
-                         dropout_probabilities, use_batch_norm)
+
+        super().__init__(data, hidden_layer_sizes, dropout_probabilities, use_batch_norm,
+                         model_name, log_tensorboard, save_model, overwrite)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
@@ -222,10 +288,18 @@ class CNNModel(BaseModel):
 class LSTMModel(BaseModel):
     def __init__(self, data: UrbanSoundData,
                  hidden_layer_sizes: Sequence[int],
-                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None):
+                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
+                 use_batch_norm: bool = False, model_name: str = None,
+                 log_tensorboard: bool = False, save_model: bool = True,
+                 overwrite: bool = False):
+
         # LSTMs do not work well with batch norm
+        if use_batch_norm:
+            print("LSTMs do not work with batch_norm, setting use_batch_norm to False")
+
         super().__init__(data, hidden_layer_sizes,
-                         dropout_probabilities, use_batch_norm=False)
+                         dropout_probabilities, False, model_name,
+                         log_tensorboard, save_model, overwrite)
 
     def _model(self, input_shape: Tuple[int]) -> Model:
         model = Sequential()
@@ -240,10 +314,63 @@ class LSTMModel(BaseModel):
 
             if dropout_proba:
                 model.add(Dropout(dropout_proba))
+
         if len(self.hidden_layer_sizes) == 1:
             model.add(LSTM(self.hidden_layer_sizes[-1], input_shape=input_shape))
         else:
             model.add(LSTM(self.hidden_layer_sizes[-1]))
+
+        if self.dropout_probabilities[-1]:
+            model.add(Dropout(self.dropout_probabilities[-1]))
+
+        model.add(Dense(NUM_CLASSES, activation="softmax"))
+        model.compile(optimizer="adam",
+                      loss="categorical_crossentropy",
+                      metrics=["categorical_accuracy"])
+        return model
+
+    def _process_features(self, features: np.ndarray) -> np.ndarray:
+        """For LSTMs, the input should be of shape (batch_size, time steps, input_dim).
+        That means we need to swap the 2nd and 3rd axis.
+        """
+        return np.swapaxes(features, 1, 2)
+
+
+class RNNModel(BaseModel):
+    def __init__(self, data: UrbanSoundData,
+                 hidden_layer_sizes: Sequence[int],
+                 dropout_probabilities: Optional[Sequence[Optional[float]]] = None,
+                 use_batch_norm: bool = False, model_name: str = None,
+                 log_tensorboard: bool = False, save_model: bool = True,
+                 overwrite: bool = False):
+
+        # LSTMs do not work well with batch norm
+        if use_batch_norm:
+            print("RNNs do not work with batch_norm, setting use_batch_norm to False")
+
+        super().__init__(data, hidden_layer_sizes,
+                         dropout_probabilities, False, model_name,
+                         log_tensorboard, save_model, overwrite)
+
+    def _model(self, input_shape: Tuple[int]) -> Model:
+        model = Sequential()
+
+        for layer_idx, (layer_size, dropout_proba) in enumerate(
+                zip(self.hidden_layer_sizes[:-1], self.dropout_probabilities[:-1])):
+            if layer_idx == 0:
+                model.add(SimpleRNN(layer_size, return_sequences=True,
+                               input_shape=input_shape))
+            else:
+                model.add(SimpleRNN(layer_size, return_sequences=True))
+
+            if dropout_proba:
+                model.add(Dropout(dropout_proba))
+
+        if len(self.hidden_layer_sizes) == 1:
+            model.add(SimpleRNN(self.hidden_layer_sizes[-1], input_shape=input_shape))
+        else:
+            model.add(SimpleRNN(self.hidden_layer_sizes[-1]))
+
         if self.dropout_probabilities[-1]:
             model.add(Dropout(self.dropout_probabilities[-1]))
 
